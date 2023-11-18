41c8b552749c5d2f410beb9b356bb769b89c67f0
Mauli1312 <114509977+Mauli1312@users.noreply.github.com>
2023-11-18 20:05:42 +0530

    Update hotel_room_occupancy.py

--- hospitality/hotels/report/hotel_room_occupancy/hotel_room_occupancy.py
+++ hospitality/hotels/report/hotel_room_occupancy/hotel_room_occupancy.py
@@ -1,34 +1,37 @@
 # Copyright (c) 2013, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-
 import frappe
 from frappe import _
 from frappe.utils import add_days, date_diff
-
 from hospitality.hotels.doctype.hotel_room_reservation.hotel_room_reservation import get_rooms_booked
 
-
 def execute(filters=None):
-	columns = get_columns(filters)
-	data = get_data(filters)
-	return columns, data
+    columns = get_columns()
+    data = get_data(filters)
+    return columns, data
 
-def get_columns(filters):
-	columns = [
-		dict(label=_("Room Type"), fieldname="room_type"),
-		dict(label=_("Rooms Booked"), fieldtype="Int")
-	]
-	return columns
+def get_columns():
+    columns = [
+        dict(label=_("Room Type"), fieldname="room_type"),
+        dict(label=_("Rooms Booked"), fieldtype="Int")
+    ]
+    return columns
 
 def get_data(filters):
-	out = []
-	for room_type in frappe.get_all('Hotel Room Type'):
-		total_booked = 0
-		for i in range(date_diff(filters.to_date, filters.from_date)):
-			day = add_days(filters.from_date, i)
-			total_booked += get_rooms_booked(room_type.name, day)
+    out = []
+    room_types = frappe.get_all('Hotel Room Type')
 
-		out.append([room_type.name, total_booked])
+    for room_type in room_types:
+        total_booked = calculate_total_booked(room_type.name, filters.from_date, filters.to_date)
+        out.append([room_type.name, total_booked])
 
-	return out
+    return out 
+
+def calculate_total_booked(room_type, from_date, to_date):
+    total_booked = 0
+    for i in range(date_diff(to_date, from_date)):
+        day = add_days(from_date, i)
+        total_booked += get_rooms_booked(room_type, day)
+
+    return total_booked
