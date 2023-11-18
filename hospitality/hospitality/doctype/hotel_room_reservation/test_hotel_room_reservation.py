74542ffdf1c5e62e94df6b327a0b132fde639703
Mauli1312 <114509977+Mauli1312@users.noreply.github.com>
2023-11-18 19:34:34 +0530

    Update test_hotel_room_reservation.py

--- hospitality/hospitality/doctype/hotel_room_reservation/test_hotel_room_reservation.py
+++ hospitality/hospitality/doctype/hotel_room_reservation/test_hotel_room_reservation.py
@@ -2,64 +2,73 @@
 # See license.txt
 
 import unittest
-
 import frappe
 
 from hospitality.hospitality.doctype.hotel_room_reservation.hotel_room_reservation import (
-	HotelRoomPricingNotSetError,
-	HotelRoomUnavailableError,
+    HotelRoomPricingNotSetError,
+    HotelRoomUnavailableError,
 )
 
 test_dependencies = ["Hotel Room Package", "Hotel Room Pricing", "Hotel Room"]
 
+
 class TestHotelRoomReservation(unittest.TestCase):
-	def setUp(self):
-		frappe.db.sql("delete from `tabHotel Room Reservation`")
-		frappe.db.sql("delete from `tabHotel Room Reservation Item`")
+    def setUp(self):
+        self.cleanup_reservations()
 
-	def test_reservation(self):
-		reservation = make_reservation(
-			from_date="2017-01-01",
-			to_date="2017-01-03",
-			items=[
-				dict(item="Basic Room with Dinner", qty=2)
-			]
-		)
-		reservation.insert()
-		self.assertEqual(reservation.net_total, 48000)
+    def tearDown(self):
+        self.cleanup_reservations()
 
-	def test_price_not_set(self):
-		reservation = make_reservation(
-			from_date="2016-01-01",
-			to_date="2016-01-03",
-			items=[
-				dict(item="Basic Room with Dinner", qty=2)
-			]
-		)
-		self.assertRaises(HotelRoomPricingNotSetError, reservation.insert)
+    def cleanup_reservations(self):
+        reservations = frappe.get_all("Hotel Room Reservation", filters={}, fields=["name"])
+        for reservation in reservations:
+            frappe.delete_doc("Hotel Room Reservation", reservation.name)
 
-	def test_room_unavailable(self):
-		reservation = make_reservation(
-			from_date="2017-01-01",
-			to_date="2017-01-03",
-			items=[
-				dict(item="Basic Room with Dinner", qty=2),
-			]
-		)
-		reservation.insert()
+    def test_reservation(self):
+        reservation = make_reservation(
+            from_date="2017-01-01",
+            to_date="2017-01-03",
+            items=[
+                dict(item="Basic Room with Dinner", qty=2)
+            ]
+        )
+        reservation.insert()
+        self.assertEqual(reservation.net_total, 48000, "Net total calculation is incorrect")
 
-		reservation = make_reservation(
-			from_date="2017-01-01",
-			to_date="2017-01-03",
-			items=[
-				dict(item="Basic Room with Dinner", qty=20),
-			]
-		)
-		self.assertRaises(HotelRoomUnavailableError, reservation.insert)
+    def test_price_not_set(self):
+        reservation = make_reservation(
+            from_date="2016-01-01",
+            to_date="2016-01-03",
+            items=[
+                dict(item="Basic Room with Dinner", qty=2)
+            ]
+        )
+        with self.assertRaises(HotelRoomPricingNotSetError):
+            reservation.insert()
+
+    def test_room_unavailable(self):
+        make_reservation(
+            from_date="2017-01-01",
+            to_date="2017-01-03",
+            items=[
+                dict(item="Basic Room with Dinner", qty=2),
+            ]
+        ).insert()
+
+        reservation = make_reservation(
+            from_date="2017-01-01",
+            to_date="2017-01-03",
+            items=[
+                dict(item="Basic Room with Dinner", qty=20),
+            ]
+        )
+        with self.assertRaises(HotelRoomUnavailableError):
+            reservation.insert()
+
 
 def make_reservation(**kwargs):
-	kwargs["doctype"] = "Hotel Room Reservation"
-	if not "guest_name" in kwargs:
-		kwargs["guest_name"] = "Test Guest"
-	doc = frappe.get_doc(kwargs)
-	return doc
+    kwargs["doctype"] = "Hotel Room Reservation"
+    if "guest_name" not in kwargs:
+        kwargs["guest_name"] = "Test Guest"
+    doc = frappe.get_doc(kwargs)
+    return doc
