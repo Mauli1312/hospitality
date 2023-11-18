c2d69e4c5049406c8836bdb41a044e5f070dd3e3
Mauli1312 <114509977+Mauli1312@users.noreply.github.com>
2023-11-18 19:19:36 +0530

    Update hotel_room_pricing.py

--- hospitality/hospitality/doctype/hotel_room_pricing/hotel_room_pricing.py
+++ hospitality/hospitality/doctype/hotel_room_pricing/hotel_room_pricing.py
@@ -1,9 +1,16 @@
 # Copyright (c) 2017, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-
 from frappe.model.document import Document
 
-
 class HotelRoomPricing(Document):
-	pass
+    def validate(self):
+        # Example validation: Ensure that the pricing is greater than 0
+        if self.price <= 0:
+            frappe.throw("Price must be greater than 0.")
+
+    def calculate_discounted_price(self):
+        # Example custom method: Calculate discounted price
+        if self.discount_percentage:
+            discounted_price = self.price - (self.price * (self.discount_percentage / 100))
+            self.discounted_price = discounted_price
