529a4a8989d482fe48f66c5d8dc55fb014046bf7
Mauli1312 <114509977+Mauli1312@users.noreply.github.com>
2023-11-18 20:15:41 +0530

    Update restaurant_order_entry.js

--- hospitality/restaurant/doctype/restaurant_order_entry/restaurant_order_entry.js
+++ hospitality/restaurant/doctype/restaurant_order_entry/restaurant_order_entry.js
@@ -15,13 +15,13 @@ frappe.ui.form.on('Restaurant Order Entry', {
 		frm.set_query('add_item', get_item_query);
 	},
 	onload_post_render: function(frm) {
-		if(!frm.item_selector) {
+		if (!frm.item_selector) {
 			frm.item_selector = new erpnext.ItemSelector({
 				frm: frm,
 				item_field: 'item',
 				item_query: 'hospitality.restaurant.doctype.restaurant_order_entry.restaurant_order_entry.item_query_restaurant',
 				get_filters: () => {
-					return {table: frm.doc.restaurant_table};
+					return { table: frm.doc.restaurant_table };
 				}
 			});
 		}
@@ -29,13 +29,13 @@ frappe.ui.form.on('Restaurant Order Entry', {
 		let $input = frm.get_field('add_item').$input;
 
 		$input.on('keyup', function(e) {
-			if (e.which===13) {
+			if (e.which === 13) {
 				if (frm.clear_item_timeout) {
-					clearTimeout (frm.clear_item_timeout);
+					clearTimeout(frm.clear_item_timeout);
 				}
 
-				// clear the item input so user can enter a new item
-				frm.clear_item_timeout = setTimeout (() => {
+				// clear the item input so the user can enter a new item
+				frm.clear_item_timeout = setTimeout(() => {
 					frm.set_value('add_item', '');
 				}, 1000);
 
@@ -45,7 +45,7 @@ frappe.ui.form.on('Restaurant Order Entry', {
 
 				var added = false;
 				(frm.doc.items || []).forEach((d) => {
-					if (d.item===item) {
+					if (d.item === item) {
 						d.qty += 1;
 						added = true;
 					}
@@ -54,10 +54,10 @@ frappe.ui.form.on('Restaurant Order Entry', {
 				return frappe.run_serially([
 					() => {
 						if (!added) {
-							return frm.add_child('items', {item: item, qty: 1});
+							return frm.add_child('items', { item: item, qty: 1 });
 						}
 					},
-					() => frm.get_field("items").refresh()
+					() => frm.get_field('items').refresh(),
 				]);
 			}
 		});
@@ -89,11 +89,11 @@ frappe.ui.form.on('Restaurant Order Entry', {
 		return frappe.call({
 			method: 'hospitality.restaurant.doctype.restaurant_order_entry.restaurant_order_entry.get_invoice',
 			args: {
-				table: frm.doc.restaurant_table
+				table: frm.doc.restaurant_table,
 			},
 			callback: (r) => {
 				frm.events.set_invoice_items(frm, r);
-			}
+			},
 		});
 	},
 	sync: function(frm) {
@@ -101,62 +101,64 @@ frappe.ui.form.on('Restaurant Order Entry', {
 			method: 'hospitality.restaurant.doctype.restaurant_order_entry.restaurant_order_entry.sync',
 			args: {
 				table: frm.doc.restaurant_table,
-				items: frm.doc.items
+				items: frm.doc.items,
 			},
 			callback: (r) => {
 				frm.events.set_invoice_items(frm, r);
-				frappe.show_alert({message: __('Saved'), indicator: 'green'});
-			}
+				frappe.show_alert({ message: __('Saved'), indicator: 'green' });
+			},
 		});
-
 	},
 	make_invoice: function(frm) {
 		frm.trigger('sync').then(() => {
-			frappe.prompt([
-				{
-					fieldname: 'customer',
-					label: __('Customer'),
-					fieldtype: 'Link',
-					reqd: 1,
-					options: 'Customer',
-					'default': frm.invoice.customer
-				},
-				{
-					fieldname: 'mode_of_payment',
-					label: __('Mode of Payment'),
-					fieldtype: 'Link',
-					reqd: 1,
-					options: 'Mode of Payment',
-					'default': frm.mode_of_payment || ''
-				}
-			], (data) => {
-				// cache this for next entry
-				frm.mode_of_payment = data.mode_of_payment;
-				return frappe.call({
-					method: 'hospitality.restaurant.doctype.restaurant_order_entry.restaurant_order_entry.make_invoice',
-					args: {
-						table: frm.doc.restaurant_table,
-						customer: data.customer,
-						mode_of_payment: data.mode_of_payment
+			frappe.prompt(
+				[
+					{
+						fieldname: 'customer',
+						label: __('Customer'),
+						fieldtype: 'Link',
+						reqd: 1,
+						options: 'Customer',
+						default: frm.invoice.customer,
 					},
-					callback: (r) => {
-						frm.set_value('last_sales_invoice', r.message);
-						frm.trigger('clear');
-					}
-				});
-			},
-			__("Select Customer"));
+					{
+						fieldname: 'mode_of_payment',
+						label: __('Mode of Payment'),
+						fieldtype: 'Link',
+						reqd: 1,
+						options: 'Mode of Payment',
+						default: frm.mode_of_payment || '',
+					},
+				],
+				(data) => {
+					// cache this for the next entry
+					frm.mode_of_payment = data.mode_of_payment;
+					return frappe.call({
+						method: 'hospitality.restaurant.doctype.restaurant_order_entry.restaurant_order_entry.make_invoice',
+						args: {
+							table: frm.doc.restaurant_table,
+							customer: data.customer,
+							mode_of_payment: data.mode_of_payment,
+						},
+						callback: (r) => {
+							frm.set_value('last_sales_invoice', r.message);
+							frm.trigger('clear');
+						},
+					});
+				},
+				__('Select Customer')
+			);
 		});
 	},
 	set_invoice_items: function(frm, r) {
 		let invoice = r.message;
 		frm.doc.items = [];
 		(invoice.items || []).forEach((d) => {
-			frm.add_child('items', {item: d.item_code, qty: d.qty, rate: d.rate});
+			frm.add_child('items', { item: d.item_code, qty: d.qty, rate: d.rate });
 		});
 		frm.set_value('grand_total', invoice.grand_total);
 		frm.set_value('last_sales_invoice', invoice.name);
 		frm.invoice = invoice;
 		frm.refresh();
-	}
+	},
 });
