89523381bdfc019ff6b9e38a4b1b9a840716c97b
Mauli1312 <114509977+Mauli1312@users.noreply.github.com>
2023-11-18 20:20:57 +0530

    Update restaurant_table.py

--- hospitality/restaurant/doctype/restaurant_table/restaurant_table.py
+++ hospitality/restaurant/doctype/restaurant_table/restaurant_table.py
@@ -1,14 +1,14 @@
 # Copyright (c) 2017, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-
 import re
-
 from frappe.model.document import Document
 from frappe.model.naming import make_autoname
 
-
 class RestaurantTable(Document):
-	def autoname(self):
-		prefix = re.sub('-+', '-', self.restaurant.replace(' ', '-'))
-		self.name = make_autoname(prefix + '-.##')
+    def autoname(self):
+        # Replace spaces with hyphens and ensure single hyphen between words
+        prefix = re.sub(r'\s+', '-', self.restaurant)
+        
+        # Generate an autoname using the formatted prefix
+        self.name = make_autoname(f'{prefix}-.##')
