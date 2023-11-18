2ac3e9ddde4510604ee0be630a60dfd817e71fda
Mauli1312 <114509977+Mauli1312@users.noreply.github.com>
2023-11-18 19:25:54 +0530

    Update test_hotel_room_pricing_package.py

--- hospitality/hospitality/doctype/hotel_room_pricing_package/test_hotel_room_pricing_package.py
+++ hospitality/hospitality/doctype/hotel_room_pricing_package/test_hotel_room_pricing_package.py
@@ -2,7 +2,26 @@
 # See license.txt
 
 import unittest
-
+from frappe.utils import getdate
+from frappe.utils import add_days
+from frappe.utils import add_months
+from frappe.utils import add_years
+from frappe.utils import get_last_day
 
 class TestHotelRoomPricingPackage(unittest.TestCase):
-	pass
+    def test_valid_total_price(self):
+        # Add your test logic here
+        pricing_package = frappe.get_doc("Hotel Room Pricing Package", "Your Package Name")
+        self.assertTrue(pricing_package.is_valid_total_price())
+
+    def test_invalid_total_price(self):
+        # Add another test case
+        pricing_package = frappe.get_doc("Hotel Room Pricing Package", "Your Package Name")
+        pricing_package.total_price = -500  # An invalid total price
+        self.assertFalse(pricing_package.is_valid_total_price())
+
+    def test_calculate_discounted_price(self):
+        # Add another test case
+        pricing_package = frappe.get_doc("Hotel Room Pricing Package", "Your Package Name")
+        discounted_price = pricing_package.calculate_discounted_price()
+        self.assertEqual(discounted_price, expected_discounted_price)
