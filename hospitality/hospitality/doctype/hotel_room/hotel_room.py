b2ffe0fd524dab672238f6e8bf4bf0d4a0f70660
Mauli1312 <114509977+Mauli1312@users.noreply.github.com>
2023-11-18 18:16:38 +0530

    Update hotel_room.py

--- hospitality/hospitality/doctype/hotel_room/hotel_room.py
+++ hospitality/hospitality/doctype/hotel_room/hotel_room.py
@@ -1,13 +1,12 @@
 # Copyright (c) 2017, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-
 import frappe
 from frappe.model.document import Document
 
-
 class HotelRoom(Document):
-	def validate(self):
-		if not self.capacity:
-			self.capacity, self.extra_bed_capacity = frappe.db.get_value('Hotel Room Type',
-					self.hotel_room_type, ['capacity', 'extra_bed_capacity'])
+    def validate(self):
+        if not self.capacity:
+            room_type = frappe.get_doc('Hotel Room Type', self.hotel_room_type)
+            self.capacity = room_type.capacity if room_type else None
+            self.extra_bed_capacity = room_type.extra_bed_capacity if room_type else None
