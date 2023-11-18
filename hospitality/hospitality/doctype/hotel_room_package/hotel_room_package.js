254857fdde4e838cf42c7e4b2a02cef883caefca
Mauli1312 <114509977+Mauli1312@users.noreply.github.com>
2023-11-18 18:20:45 +0530

    Update hotel_room_package.js

--- hospitality/hospitality/doctype/hotel_room_package/hotel_room_package.js
+++ hospitality/hospitality/doctype/hotel_room_package/hotel_room_package.js
@@ -2,22 +2,26 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Hotel Room Package', {
-	hotel_room_type: function(frm) {
-		if (frm.doc.hotel_room_type) {
-			frappe.model.with_doc('Hotel Room Type', frm.doc.hotel_room_type, () => {
-				let hotel_room_type = frappe.get_doc('Hotel Room Type', frm.doc.hotel_room_type);
+    hotel_room_type: function(frm) {
+        if (frm.doc.hotel_room_type) {
+            frappe.model.with_doc('Hotel Room Type', frm.doc.hotel_room_type, () => {
+                try {
+                    let hotel_room_type = frappe.get_doc('Hotel Room Type', frm.doc.hotel_room_type);
 
-				// reset the amenities
-				frm.doc.amenities = [];
+                    // reset the amenities
+                    frm.doc.amenities = [];
 
-				for (let amenity of hotel_room_type.amenities) {
-					let d = frm.add_child('amenities');
-					d.item = amenity.item;
-					d.billable = amenity.billable;
-				}
+                    for (let amenity of hotel_room_type.amenities) {
+                        let d = frm.add_child('amenities');
+                        d.item = amenity.item;
+                        d.billable = amenity.billable;
+                    }
 
-				frm.refresh();
-			});
-		}
-	}
+                    frm.refresh();
+                } catch (e) {
+                    frappe.msgprint(__("Error loading Hotel Room Type: {0}", [e.message]));
+                }
+            });
+        }
+    }
 });
