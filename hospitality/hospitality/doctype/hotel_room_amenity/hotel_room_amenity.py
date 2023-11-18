bbdb6e405d520539a55e49760b92c182d595a116
Mauli1312 <114509977+Mauli1312@users.noreply.github.com>
2023-11-18 18:19:17 +0530

    Update hotel_room_amenity.py

--- hospitality/hospitality/doctype/hotel_room_amenity/hotel_room_amenity.py
+++ hospitality/hospitality/doctype/hotel_room_amenity/hotel_room_amenity.py
@@ -1,9 +1,16 @@
 # Copyright (c) 2017, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-
 from frappe.model.document import Document
-
+import frappe
 
 class HotelRoomAmenity(Document):
-	pass
+    # New fields
+    def validate(self):
+        # Example validation: Ensure that the quantity is positive
+        if self.quantity <= 0:
+            frappe.throw("Quantity must be a positive number.")
+
+    # Custom method to calculate total cost
+    def calculate_total_cost(self):
+        self.total_cost = self.rate * self.quantity
